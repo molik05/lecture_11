fd1ff671e1932bea82cdf569e5933896f737d426
molik05 <xmolik05@vutbr.cz>
2021-04-19 10:54:11 +0200

    Second task done

--- searching.py
+++ searching.py
@@ -20,9 +20,23 @@ def read_data(file_name, field):
 
     return data[field]
 
+
+def liner_search(sequence, number):
+    index = 0
+    positions = []
+    while index < len(sequence):
+        if sequence[index] == number:
+            positions.append(index)
+        index = index + 1
+    count = len(positions)
+    return {"positions": positions, "count": count}
+
+
 def main():
     sequential_data = read_data("sequential.json", "unordered_numbers")
     print(sequential_data)
+    search = liner_search(sequential_data, 5)
+    print(search)
 
 if __name__ == '__main__':
     main()
